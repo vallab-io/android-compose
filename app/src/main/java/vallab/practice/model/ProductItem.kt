57b3d0cedda1a1c808cd3ae6b2e77c447aa65631
KKS <ksiki1@naver.com>
2026-06-02 02:16:41 +0900

    fix: 상품명 한 줄 넘어갈 때 생략 설정

--- app/src/main/java/vallab/practice/model/ProductItem.kt
+++ app/src/main/java/vallab/practice/model/ProductItem.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,6 +47,8 @@ fun ProductItem(
         ) {
             Text(
                 text = product.name,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
                 fontWeight = FontWeight.Bold,
                 lineHeight = 18.sp
             )
