eab678bc50c50c8239740b52762f06ec049e339e
KKS <ksiki1@naver.com>
2026-06-05 19:05:05 +0900

    feat: ButtonComponent 분리

--- app/src/main/java/vallab/practice/screen/CartDetailScreen.kt
+++ app/src/main/java/vallab/practice/screen/CartDetailScreen.kt
@@ -2,14 +2,11 @@ package vallab.practice.screen
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -21,17 +18,14 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import vallab.practice.R
+import vallab.practice.component.ButtonComponent
 import vallab.practice.component.CartDetailItem
 import vallab.practice.model.Cart
 import vallab.practice.model.dummyProducts
-import vallab.practice.ui.theme.Blue100
 import vallab.practice.ui.theme.PracticeTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -60,22 +54,10 @@ fun CartDetailScreen(
             )
         },
         bottomBar = {
-            Button(
+            ButtonComponent(
                 onClick = {},
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .navigationBarsPadding(),
-                shape = RectangleShape,
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Blue100,
-                    contentColor = Color.White
-                )
-            ) {
-                Text(
-                    text = stringResource(R.string.button_order, totalPrice),
-                    fontSize = 22.sp
-                )
-            }
+                text = stringResource(R.string.button_order, totalPrice)
+            )
         }
     ) { innerPadding ->
         LazyColumn(modifier = modifier.padding(innerPadding)) {
